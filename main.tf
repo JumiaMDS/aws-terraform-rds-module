0204d0a26e3bf80b54b75f23b81b556da7786bd6
Sergio Teixeira <sergiopintoteixeira@gmail.com>
2020-02-20 12:41:03 +0100

    init

---
+++ main.tf
@@ -0,0 +1,77 @@
+resource "aws_db_subnet_group" "main_db_subnet_group" {
+  name        = "${var.environment}-${var.rds_instance_name}-subnetgrp"
+  description = "${var.environment}-${var.rds_instance_name} RDS subnet group"
+  subnet_ids  = var.subnet_ids
+
+  tags = {
+    terraform     = "true"
+  }
+
+  lifecycle {
+    ignore_changes = [name, description, tags]
+  }
+}
+
+resource "aws_db_instance" "main_rds_instance" {
+  identifier                   = var.rds_instance_name
+  allocated_storage            = var.rds_allocated_storage
+  deletion_protection          = var.rds_deletion_protection
+  engine                       = var.rds_engine_type
+  engine_version               = var.rds_engine_version
+  instance_class               = var.rds_instance_class
+  name                         = var.database_name
+  username                     = var.database_user
+  snapshot_identifier          = var.snapshot_identifier
+  password                     = var.database_password
+  multi_az                     = var.rds_is_multi_az
+  storage_type                 = var.rds_storage_type
+  final_snapshot_identifier    = "${var.environment}-${replace(timestamp(), ":", "-")}-Final-Snapshot"
+  apply_immediately            = var.apply_immediately
+  parameter_group_name         = var.db_parameter_group
+  storage_encrypted            = false
+  skip_final_snapshot          = true
+  auto_minor_version_upgrade   = true
+  copy_tags_to_snapshot        = true
+  backup_retention_period      = 15
+  maintenance_window           = "tue:00:04-tue:00:34"
+  backup_window                = "23:32-00:02"
+  performance_insights_enabled = var.performance_insights
+
+  // Because we're assuming a VPC, we use this option, but only one SG id
+  vpc_security_group_ids = var.rds_security_group_id
+
+  // We're creating a subnet group in the module and passing in the name
+  db_subnet_group_name = aws_db_subnet_group.main_db_subnet_group.name
+
+  tags = {
+    business-unit = "JMDS"
+    application   = "rds-postgres"
+  }
+
+  lifecycle {
+    ignore_changes = [final_snapshot_identifier]
+  }
+}
+
+resource "aws_db_instance" "read_replica_rds_instance" {
+  identifier                 = "${var.rds_instance_name}-replica-${count.index + 1}"
+  allocated_storage          = var.rds_allocated_storage
+  engine                     = var.rds_engine_type
+  engine_version             = var.rds_engine_version
+  instance_class             = var.read_replica_instance_type
+  storage_type               = var.rds_storage_type
+  replicate_source_db        = aws_db_instance.main_rds_instance.identifier
+  parameter_group_name       = var.db_parameter_group
+  skip_final_snapshot        = true
+  auto_minor_version_upgrade = true
+
+  // Because we're assuming a VPC, we use this option, but only one SG id
+  vpc_security_group_ids = var.rds_security_group_id
+
+  count = var.read_replica
+
+  tags = {
+    business-unit = "JMDS"
+    application   = "rds-postgres"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,70 @@
+//
+// Module: tf_aws_rds
+//
+
+// RDS Instance Variables
+
+variable "rds_instance_name" {}
+
+variable "performance_insights" {
+  default = true
+}
+
+variable "snapshot_identifier" {}
+
+variable "rds_is_multi_az" {
+  default = "false"
+}
+
+variable "rds_deletion_protection" {
+  default = true
+}
+
+variable "rds_storage_type" {
+  default = "gp2"
+}
+
+variable "rds_allocated_storage" {
+  description = "The allocated storage in GBs"
+
+  // You just give it the number, e.g. 10
+}
+
+variable "rds_engine_type" {
+  // Valid types are  // - mysql  // - postgres  // - oracle-*  // - sqlserver-*  // See http://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html  // --engine
+}
+
+variable "rds_engine_version" {
+  // For valid engine versions, see:  // See http://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html  // --engine-version
+}
+
+variable "rds_instance_class" {}
+
+variable "database_name" {
+  description = "The name of the database to create"
+}
+
+variable "database_user" {}
+variable "database_password" {}
+
+variable "rds_security_group_id" {
+  type = set(string)
+}
+
+variable "db_parameter_group" {
+  default = "default.mysql5.6"
+}
+
+// RDS Subnet Group Variables
+variable "environment" {}
+
+variable "apply_immediately" {}
+variable "read_replica" {}
+
+variable "read_replica_instance_type" {
+  default = ""
+}
+
+variable "subnet_ids" {
+  type = set(string)
+}
